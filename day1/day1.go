29ee1ff0e5f40ceab82e91d3741d537067286d2c
nmill <nmill@DESKTOP-E7RII8U>
2024-12-13 18:06:38 -0600

    Finished Day of AOC2024

--- day1/day1.go
+++ day1/day1.go
@@ -33,12 +33,80 @@ func parseData(data []string) ([]int, []int) {
 	}
 	return larr, rarr
 }
+
+func merge(left []int, right []int) []int {
+	final := []int{}
+	i := 0
+	j := 0
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			final = append(final, left[i])
+			i++
+		} else {
+			final = append(final, right[j])
+			j++
+		}
+	}
+	for ; i < len(left); i++ {
+		final = append(final, left[i])
+	}
+	for ; j < len(right); j++ {
+		final = append(final, right[j])
+	}
+	return final
+}
+
+func sort(data []int) []int {
+	if len(data) < 2 {
+		return data
+	}
+	left := sort(data[:len(data)/2])
+	right := sort(data[len(data)/2:])
+	return merge(left, right)
+}
+
+func difference(larr []int, rarr []int) int {
+	result := 0
+	for i := 0; i < len(larr); i++ {
+		diff := larr[i] - rarr[i]
+		if diff < 0 {
+			diff = diff * -1
+		}
+		result = result + diff
+	}
+	return result
+}
+
+func hash(data []int) map[int]int {
+	m := make(map[int]int)
+	for i := 0; i < len(data); i++ {
+		m[data[i]] = m[data[i]] + 1
+	}
+	return m
+}
+
+func sim(larr []int, count map[int]int) int {
+	result := 0
+	for i := 0; i < len(larr); i++ {
+		result = result + larr[i]*count[larr[i]]
+	}
+	return result
+}
+
 func main() {
-	data := readFile("test_input.txt")
-	trimmedData := strings.Trim(string(data), "\n")
-	splitData := strings.Split(trimmedData, "\n")
+	data := readFile("input.txt")
+	trimmedData := strings.Trim(string(data), "\r\n")
+	splitData := strings.Split(trimmedData, "\r\n")
 	larr, rarr := parseData(splitData)
-	fmt.Println(larr)
-	fmt.Println(rarr)
+	countarr := hash(rarr)
+	result := sim(larr, countarr)
+	fmt.Printf("Final similarity %d\n", result)
+
+	/**
+	*larr = sort(larr)
+	*rarr = sort(rarr)
+	diff := difference(larr, rarr)
+	fmt.Printf("Final difference: %d\n", diff)
+	**/
 
 }
